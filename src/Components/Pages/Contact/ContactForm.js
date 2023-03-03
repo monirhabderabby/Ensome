190e24f13a890a4666b4827a966b676561b8c741
Monir <monirhrabby.personal@gmail.com>
2023-03-03 11:18:21 +0600

    added responsive contact page

--- src/Components/Pages/Contact/ContactForm.js
+++ src/Components/Pages/Contact/ContactForm.js
@@ -7,22 +7,22 @@ import { PrimaryButton } from "../../Shared/buttons/PrimaryButton";
 export const ContactForm = () => {
     return (
         <div className="max-w-[1500px] mx-auto px-[16px]">
-            <div className="w-full flex items-start justify-between">
-                <div className="text-[black] text-[80px] font-manrope font-extrabold flex-1">
-                    How can we <br /> <span className="text-primary">help you?</span>
+            <div className="w-full flex flex-col md:flex-row items-start justify-between">
+                <div className="text-[black] text-[30px] md:text-[80px] font-manrope font-extrabold flex-1 mb-[38px] md:pb-0">
+                    How can we <div className="hidden md:block"></div> <span className="text-primary">help you?</span>
                 </div>
-                <div className="flex-1">
+                <div className="flex-1 w-full">
                     <form className="w-full">
-                        <div className="grid grid-cols-2 gap-x-[32px]  gap-y-[22px]">
+                        <div className="grid grid-cols-2 gap-x-[32px]  gap-y-[22px] w-full">
                             <input
                                 type="text"
                                 placeholder="Your email"
-                                className="text-gray border-[1px] border-gray rounded-[6px] py-[15px] px-[25px]"
+                                className="text-gray border-[1px] border-gray rounded-[6px] py-[15px] px-[25px] col-span-2 md:col-span-1"
                             />
                             <input
                                 type="text"
                                 placeholder="Your name"
-                                className="text-gray border-[1px] border-gray rounded-[6px] py-[15px] px-[25px]"
+                                className="text-gray border-[1px] border-gray rounded-[6px] py-[15px] px-[25px] col-span-2 md:col-span-1 w-full"
                             />
                             <input
                                 type="text"
@@ -42,7 +42,7 @@ export const ContactForm = () => {
                     </form>
                 </div>
             </div>
-            <div className="flex items-center gap-x-[47px]">
+            <div className="flex flex-col md:flex-row items-start md:items-center gap-x-[47px] gap-y-[20px] mt-[50px] md:mt-0">
                 <div className="flex flex-col justify-start">
                     <div className="flex items-center gap-x-[14px]">
                         <img src={emailIcon} alt="email" />
